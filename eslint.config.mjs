eff7611567232126544688d07c38eb078461b3a9
William Xu <wxu9750@gmail.com>
2026-06-23 20:52:07 -0400

    Ignore repo worktrees during lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 import coreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
-const eslintConfig = [...coreWebVitals, ...nextTypescript];
+const eslintConfig = [
+  {
+    ignores: [".worktrees/**"]
+  },
+  ...coreWebVitals,
+  ...nextTypescript
+];
 
 export default eslintConfig;
